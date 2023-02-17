9936407afcb3f7c13f4cf75a7f0c9d4c9c570e73
ClumsyIsNotReal <106121696+ClumsyIsNotReal@users.noreply.github.com>
2023-02-17 16:49:19 +0000

    Add files via upload

--- src/main/kotlin/me/untouchedodin0/kotlin/menu/Menu.kt
+++ src/main/kotlin/me/untouchedodin0/kotlin/menu/Menu.kt
@@ -21,8 +21,10 @@
 
 package me.untouchedodin0.kotlin.menu
 
+import me.untouchedodin0.kotlin.mine.data.MineData
 import me.untouchedodin0.kotlin.utils.BuilderUtils
 import me.untouchedodin0.privatemines.PrivateMines
+import me.untouchedodin0.privatemines.mine.Mine
 import me.untouchedodin0.privatemines.utils.ActionUtils
 import me.untouchedodin0.privatemines.utils.Utils
 import org.bukkit.Bukkit
@@ -35,6 +37,7 @@ import redempt.redlib.config.annotations.ConfigPath
 import redempt.redlib.inventorygui.InventoryGUI
 import redempt.redlib.inventorygui.ItemButton
 import redempt.redlib.itemutils.ItemBuilder
+import java.util.*
 
 @ConfigMappable
 class Menu {
@@ -50,28 +53,34 @@ class Menu {
         val slots = rows * 9
         val inventoryGUI = InventoryGUI(slots, Utils.colorBukkit(title))
         val mineStorage = privateMines.mineStorage
+        val mine = mineStorage.get(player)
         var buttonSlot = 0
 
-        if (name.equals("publicMines", true)) {
-            mineStorage.mines.forEach { (uuid, mine) ->
-                run {
-                    val name = "${Bukkit.getOfflinePlayer(uuid).name}'s Mine"
-                    val tax = mine.mineData.tax
 
-                    val itemButton = ItemButton.create(BuilderUtils().itemBuilder(Material.BEACON) {
-                        setName(name)
-                        addLore("${ChatColor.GRAY}Click to teleport")
-                        addLore("${ChatColor.GRAY}Tax $tax%")
-                    }) { event: InventoryClickEvent? ->
+        if (name.equals("publicMines", true)) {
+            if (mine != null) {
+                    mineStorage.mines.forEach { (uuid, mine) ->
                         run {
-                            event?.isCancelled = true
-                            mine.teleport(player)
+                            val name = "${Bukkit.getOfflinePlayer(uuid).name}'s Mine"
+                            val tax = mine.mineData.tax
+
+                            val itemButton = ItemButton.create(BuilderUtils().itemBuilder(Material.BEACON) {
+                                setName(name)
+                                addLore("${ChatColor.GRAY}Click to teleport")
+                                addLore("${ChatColor.GRAY}Tax $tax%")
+                            }) { event: InventoryClickEvent? ->
+                                run {
+                                    event?.isCancelled = true
+                                    mine.teleport(player)
+                                }
+                            }
+                            if (mine.mineData.isOpen) {
+                                inventoryGUI.addButton(buttonSlot++, itemButton)
+                            }
                         }
                     }
-                    inventoryGUI.addButton(buttonSlot++, itemButton)
-                }
+                inventoryGUI.open(player)
             }
-            inventoryGUI.open(player)
         } else {
             items.forEach {
                 val material = it.value.material
